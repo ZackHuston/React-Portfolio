68743bd33ce4601205022af4c346a6e9cd900b28
Zack Huston <zackhuston@Zacks-MacBook-Air.local>
2023-03-03 13:49:07 -0500

    Added Second project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import "./styles/Portfolio.css"
-import dineWine from '../assets/Dine&Wine.png'
+import React from "react";
+import "./styles/Portfolio.css";
+import dineWine from "../assets/Dine&Wine.png";
+import reviewApp from "../assets/ReviewsForDays.png";
 import Github from "../assets/github2.png";
 
-
 export default function Portfolio() {
-    return (
-        <div className='portfolio'> 
-      <a href="https://terryboufis.github.io/Dine-and-Wine-App/" 
-      target="_blank" 
-      rel="noreferrer">
-        <img className="app" src={dineWine} alt="Dine&Wine app screenshot" />
+  return (
+    <div className="portfolio">
+      <a
+        href="https://terryboufis.github.io/Dine-and-Wine-App/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img className="app" src={dineWine} alt="Dine&Wine App Screenshot" />
       </a>
-      <ul className='GitHub-Link'>
-        <li><a href="https://github.com/TerryBoufis/Dine-and-Wine-App" 
-      target="_blank" 
-      rel="noreferrer">
-        <img className="github2" src={Github} alt="Github Link Logo" />
-      </a>
-      </li>
+      <ul className="GitHub-Link">
+        <li>
+          <a
+            href="https://github.com/TerryBoufis/Dine-and-Wine-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="github2" src={Github} alt="Github Link Logo" />
+          </a>
+        </li>
       </ul>
-        </div>
-    )
+      <a 
+        href="https://reviews-for-days.herokuapp.com/Links to an external site." 
+        target="_blank"
+        rel="noreferrer"
+        >
+          <img className="app2" src={reviewApp} alt="Review App Screenshot"/>
+      </a>
+      <ul className="GitHub-Link2">
+        <li>
+          <a 
+            href="https://github.com/Hayek23/Reviews-For-Days"
+            target="_blank"
+            rel="noreferrer">
+              <img className="github3" src={Github} alt="Github Link Logo"></img>
+
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
